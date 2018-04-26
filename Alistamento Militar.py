6e8c8c62a454062a9724eb41c810469a91ae4f32
voanjos <voanjos@gmail.com>
2018-04-26 13:59:29 -0300

    retirei a condicional do sexo feminino

--- Alistamento Militar.py
+++ Alistamento Militar.py
@@ -2,35 +2,21 @@
 from datetime import date
 atual = date.today().year
 
-#Definindo o sexo
-sexo = int(input('''Masculino ou Feminino
-[1] Feminino
-[2] Masculino
-1 ou 2: '''))
+#Definindo a idade:
+nasc = int(input('Digite o ano que você nasceu em 4 digitos: '))
+idade = atual - nasc
 
-fem = 1
-masc = 2
-
-if sexo == fem:
-    print('\033[35mMulheres não estão sujeitas ao Alistamento Militar Obrigatorio!'
-          '\nMas sinta-se a vontade para servir as forças armadas se desejar!')
-elif sexo == masc:
-    nasc = int(input('Ano de nascimento: '))
-
-    #Definindo a idade:
-    idade = atual - nasc
-
-    #Definindo as condicionantes:
-    #Menor:
-    if date - nasc < 18:
-        print('\033[34mVocê tem {} anos e ainda faltam {} anos para se alistar'
-              .format(idade, 18 - idade))
-    #18 anos:
-    elif date - nasc == 18:
-        print('\033[33mVocê tem {} anos e deve se alistar imediatamente!'
-              .format(idade))
-    #Maior:
-    elif date - nasc > 18:
-        print('\33[31mVocê tem {} e já deveria ter se alistado a {} anos atrás!'
-              .format(idade, idade - 18))
-        print('Seu Alistamento foi em: {}!'.format(nasc + 18))
+#Definindo as condicionantes:
+#Menor:
+if atual - nasc < 18:
+    print('\033[34mVocê tem {} anos e ainda faltam {} anos para se alistar.'
+          .format(idade, 18 - idade))
+#18 anos:
+elif atual - nasc == 18:
+    print('\033[33mVocê tem {} anos e deve se alistar este ano!'
+          .format(idade))
+#Maior:
+elif atual - nasc > 18:
+    print('\33[31mVocê tem {} e já deveria ter se alistado à {} anos atrás!'
+          .format(idade, idade - 18))
+    print('Seu ano de alistamento foi em: {}'.format(nasc + 18))
