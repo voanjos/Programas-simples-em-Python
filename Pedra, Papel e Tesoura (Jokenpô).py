ad96eb21da457846f797033faca449dbdb687332
Vinícius O. dos Anjos <38674400+voanjos@users.noreply.github.com>
2018-04-27 22:57:08 -0300

    Agora está funcionando
    Alterei (1, 3) para (0, 2)

--- Pedra, Papel e Tesoura (Jokenpô).py
+++ Pedra, Papel e Tesoura (Jokenpô).py
@@ -4,40 +4,41 @@ from time import sleep
 
 #Definindo variáveis:
 itens = ('Pedra', 'PAPEL', 'TESOURA')
-computador = randint(1, 3)
+computador = randint(0, 2)
 ##print('COMPUTADOR: {}'.format(itens[computador]))
 
 #Escolhas do Jogador e Computador:
 print('''Suas opções:
-[1] PEDRA
-[2] PAPEL
-[3] TESOURA''')
+[0] PEDRA
+[1] PAPEL
+[2] TESOURA''')
 
 #Caso o jogador escolha um número fora das opções:
 jogador = (int(input('Qual é a sua jogada? ')))
 ##print('VOCÊ: {}'.format(itens[jogador]))
-if jogador != 1 and jogador != 2 and jogador != 3:
+if jogador != 0 and jogador != 1 and jogador != 2:
     print('''\33[31mJogada Inválida!
 Você deve escolher: 
-    1 para PEDRA
-        2 para PAPEL 
-            3 para TESOURA''')
+    0 para PEDRA
+        1 para PAPEL 
+            2 para TESOURA''')
 else:
+
 #Cenário em que o computador escolhe ##PEDRA##
-    if computador == 1:
-        if jogador == 1: #Pedra
+    if computador == 0:
+        if jogador == 0: #Pedra
             print()
             print('VOCÊ: {}'.format(itens[jogador]))
             print('COMPUTADOR: {}'.format(itens[computador]))
             print()
             print('\33[33mDeu Empate!')
-        elif jogador == 2: #Papel
+        elif jogador == 1: #Papel
             print()
             print('VOCÊ: {}'.format(itens[jogador]))
             print('COMPUTADOR: {}'.format(itens[computador]))
             print()
-            print('\33[32Você VENCEU!!')
-        elif jogador == 3: #Tesoura
+            print('\33[32mVocê VENCEU!!')
+        elif jogador == 2: #Tesoura
             print()
             print('VOCÊ: {}'.format(itens[jogador]))
             print('COMPUTADOR: {}'.format(itens[computador]))
@@ -45,41 +46,41 @@ else:
             print('\33[31mVocê Perdeu.')
 
 #Cenário em que o computador escolhe ##PAPEL##
-    elif computador == 2:
-        if jogador == 1:  # Pedra
+    elif computador == 1:
+        if jogador == 0:  # Pedra
             print()
             print('VOCÊ: {}'.format(itens[jogador]))
             print('COMPUTADOR: {}'.format(itens[computador]))
             print()
             print('\33[31mVocê Perdeu.')
-        elif jogador == 2:  # Papel
+        elif jogador == 1:  # Papel
             print()
             print('VOCÊ: {}'.format(itens[jogador]))
             print('COMPUTADOR: {}'.format(itens[computador]))
             print()
             print('\33[33mDeu Empate!')
-        elif jogador == 3:  # Tesoura
+        elif jogador == 2:  # Tesoura
             print()
             print('VOCÊ: {}'.format(itens[jogador]))
             print('COMPUTADOR: {}'.format(itens[computador]))
             print()
-            print('\33[32Você VENCEU!!')
+            print('\33[32mVocê VENCEU!!')
 
 #Cenário em que o computador escolhe ##TESOURA##
-    elif computador == 3:
-        if jogador == 1:  # Pedra
+    elif computador == 2:
+        if jogador == 0:  # Pedra
             print()
             print('VOCÊ: {}'.format(itens[jogador]))
             print('COMPUTADOR: {}'.format(itens[computador]))
             print()
-            print('\33[32Você VENCEU!!')
-        elif jogador == 2:  # Papel
+            print('\33[32mVocê VENCEU!!')
+        elif jogador == 1:  # Papel
             print()
             print('VOCÊ: {}'.format(itens[jogador]))
             print('COMPUTADOR: {}'.format(itens[computador]))
             print()
             print('\33[31mVocê Perdeu.')
-        elif jogador == 3:  # Tesoura
+        elif jogador == 2:  # Tesoura
             print()
             print('VOCÊ: {}'.format(itens[jogador]))
             print('COMPUTADOR: {}'.format(itens[computador]))
