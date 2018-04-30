56cbd8ac5f672b13c4b3ee273495846d4de78549
Vinícius O. dos Anjos <38674400+voanjos@users.noreply.github.com>
2018-04-29 22:00:07 -0300

    Joken pow
    Adicionei a função SLEEP para 
    jo
    ken
    po

--- Pedra, Papel e Tesoura (Jokenpô).py
+++ Pedra, Papel e Tesoura (Jokenpô).py
@@ -3,9 +3,8 @@ from random import randint
 from time import sleep
 
 #Definindo variáveis:
-itens = ('Pedra', 'PAPEL', 'TESOURA')
+itens = ('PEDRA', 'PAPEL', 'TESOURA')
 computador = randint(0, 2)
-##print('COMPUTADOR: {}'.format(itens[computador]))
 
 #Escolhas do Jogador e Computador:
 print('''Suas opções:
@@ -15,18 +14,25 @@ print('''Suas opções:
 
 #Caso o jogador escolha um número fora das opções:
 jogador = (int(input('Qual é a sua jogada? ')))
-##print('VOCÊ: {}'.format(itens[jogador]))
+
 if jogador != 0 and jogador != 1 and jogador != 2:
     print('''\33[31mJogada Inválida!
 Você deve escolher: 
     0 para PEDRA
         1 para PAPEL 
             2 para TESOURA''')
-else:
 
+else:
 #Cenário em que o computador escolhe ##PEDRA##
     if computador == 0:
         if jogador == 0: #Pedra
+            print()
+            print('JO')
+            sleep(1)
+            print('KEN')
+            sleep(1)
+            print('PÔ!!')
+            sleep(1)
             print()
             print('VOCÊ: {}'.format(itens[jogador]))
             print('COMPUTADOR: {}'.format(itens[computador]))
@@ -34,11 +40,25 @@ else:
             print('\33[33mDeu Empate!')
         elif jogador == 1: #Papel
             print()
+            print('JO')
+            sleep(1)
+            print('KEN')
+            sleep(1)
+            print('PÔ!!')
+            sleep(1)
+            print()
             print('VOCÊ: {}'.format(itens[jogador]))
             print('COMPUTADOR: {}'.format(itens[computador]))
             print()
             print('\33[32mVocê VENCEU!!')
         elif jogador == 2: #Tesoura
+            print()
+            print('JO')
+            sleep(1)
+            print('KEN')
+            sleep(1)
+            print('PÔ!!')
+            sleep(1)
             print()
             print('VOCÊ: {}'.format(itens[jogador]))
             print('COMPUTADOR: {}'.format(itens[computador]))
@@ -49,17 +69,38 @@ else:
     elif computador == 1:
         if jogador == 0:  # Pedra
             print()
+            print('JO')
+            sleep(1)
+            print('KEN')
+            sleep(1)
+            print('PÔ!!')
+            sleep(1)
+            print()
             print('VOCÊ: {}'.format(itens[jogador]))
             print('COMPUTADOR: {}'.format(itens[computador]))
             print()
             print('\33[31mVocê Perdeu.')
         elif jogador == 1:  # Papel
             print()
+            print('JO')
+            sleep(1)
+            print('KEN')
+            sleep(1)
+            print('PÔ!!')
+            sleep(1)
+            print()
             print('VOCÊ: {}'.format(itens[jogador]))
             print('COMPUTADOR: {}'.format(itens[computador]))
             print()
             print('\33[33mDeu Empate!')
         elif jogador == 2:  # Tesoura
+            print()
+            print('JO')
+            sleep(1)
+            print('KEN')
+            sleep(1)
+            print('PÔ!!')
+            sleep(1)
             print()
             print('VOCÊ: {}'.format(itens[jogador]))
             print('COMPUTADOR: {}'.format(itens[computador]))
@@ -70,17 +111,38 @@ else:
     elif computador == 2:
         if jogador == 0:  # Pedra
             print()
+            print('JO')
+            sleep(1)
+            print('KEN')
+            sleep(1)
+            print('PÔ!!')
+            sleep(1)
+            print()
             print('VOCÊ: {}'.format(itens[jogador]))
             print('COMPUTADOR: {}'.format(itens[computador]))
             print()
             print('\33[32mVocê VENCEU!!')
         elif jogador == 1:  # Papel
             print()
+            print('JO')
+            sleep(1)
+            print('KEN')
+            sleep(1)
+            print('PÔ!!')
+            sleep(1)
+            print()
             print('VOCÊ: {}'.format(itens[jogador]))
             print('COMPUTADOR: {}'.format(itens[computador]))
             print()
             print('\33[31mVocê Perdeu.')
         elif jogador == 2:  # Tesoura
+            print()
+            print('JO')
+            sleep(1)
+            print('KEN')
+            sleep(1)
+            print('PÔ!!')
+            sleep(1)
             print()
             print('VOCÊ: {}'.format(itens[jogador]))
             print('COMPUTADOR: {}'.format(itens[computador]))
