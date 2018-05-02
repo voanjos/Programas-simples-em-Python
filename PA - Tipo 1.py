04f3997a80d723110a7c0ab56e26adbd1867af3b
Vinícius O. dos Anjos <38674400+voanjos@users.noreply.github.com>
2018-05-01 22:10:01 -0300

    Add files via upload
    Adicionei Hashtags

--- PA - Tipo 1.py
+++ PA - Tipo 1.py
@@ -1,11 +1,17 @@
+# Cabeçalho
 print()
 print('\33[1m='*25)
 print('   10 TERMOS DE UMA PA')
 print('='*25)
 print()
+
+# Variáveis
 primeiro = int(input('Primeiro Termo: '))
 razão = int(input('Razão: '))
 décimo = primeiro + (10) * razão
+
+# RANGE
+print()
 for c in range(primeiro, décimo,  razão):
     print('{}'.format(c), end=' -> ')
 print('Acabou!')
