d74186d5db5b60f5a645eec3412f0a0b84f8be15
Vinícius O. dos Anjos <38674400+voanjos@users.noreply.github.com>
2018-05-06 21:23:23 -0300

    Add files via upload

---
+++ Tabuada - Tipo 2.py
@@ -0,0 +1,11 @@
+# O programa faz a tabuada de qualquer número inteiro, quantas
+# vezes o usuário quiser, até digitar o número 0 (ZERO), que aciona o comando BREAK
+
+while True:
+    print()
+    n = int(input('\33[1mDigite para a sua tabuada: '))
+    if n == 0:
+        break
+    for c in range(1, 10):
+        print(f'\33[36m{n} x {c} = {n*c:.0f}')
+print('\33[34mFIM ;)')
